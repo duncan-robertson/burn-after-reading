2d577327767b3bfea74d027bb1fa092a9178680a
Duncan Robertson <me@duncan-robertson.com>
2017-07-19 11:25:04 -0400

    Secrets template file

---
+++ secrets.js
@@ -0,0 +1,3 @@
+module.exports.port = process.env.PORT || 'YOUR_DEFAULT_PORT'
+module.exports.ip = process.env.IP || 'YOUR_DEFAULT_IP'
+module.exports.dbPath = 'mongodb://<dbuser>:<dbpass>@hostname/database'
